df0ac6d9cbf3db036d040268240c56645bd457dc
Anton Lysenko <antonlysenko161@yandex.ru>
2024-09-22 21:14:34 +0300

    Make native modules

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,12 @@
 // TODO: write your code here
-import sum from './basic';
+import sum from './basic.js';
 
-console.log('worked');
+console.log('App worked');
 
 console.log(sum([1, 2]));
 
 // Задание import/export
-import Game, {GameSavingData, readGameSaving as loadGame, writeGameSaving as saveGame} from './game';
+import Game, {GameSavingData, readGameSaving as loadGame, writeGameSaving as saveGame} from './game.js';
 
 const game = new Game();
 game.start();
